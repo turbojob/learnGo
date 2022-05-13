1e8336530bc4a4ad64ba689ea9bd25bde9ff155a
zhaobo <xbackturbo@163.com>
2022-05-13 15:43:31 +0800

    模块使用

---
+++ src/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+import "learnGo/src/util"
+
+func main() {
+	fmt.Println("我的第一个 main程序")
+	fmt.Println(util.CalSum(10, 30))
+}
---
+++ src/util/calSum.go
@@ -0,0 +1,5 @@
+package util
+
+func CalSum(a, b int) int {
+	return a + b
+}
