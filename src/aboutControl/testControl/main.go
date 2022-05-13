0b3855d163e9ac1490fe6c0ed59b79e95553676d
zhaobo <xbackturbo@163.com>
2022-05-13 16:58:12 +0800

    控制流   查找质数程序

--- src/aboutControl/testControl/main.go
+++ src/aboutControl/testControl/main.go
@@ -18,6 +18,29 @@ func FizzBuzz() {
 		}
 	}
 }
+func findprimes(number int) bool {
+	for i := 2; i <= number-1; i++ {
+		if number%i == 0 {
+			return false
+		}
+	}
+
+	if number != 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func getPrime() {
+	fmt.Println("Prime numbers less than 20:")
+
+	for number := 1; number <= 20; number++ {
+		if findprimes(number) {
+			fmt.Printf("%v ", number)
+		}
+	}
+}
 func main() {
-	FizzBuzz()
+	getPrime()
 }
